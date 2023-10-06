52aa0a24a756bf1929da875968234e3a1b177b99
lucas-iribaren <lucas.iribaren@laplateforme.io>
2023-10-06 19:38:54 +0200

    ajout des nouveaux jobs

--- job08/get_logs.sh
+++ job08/get_logs.sh
@@ -1,6 +1,13 @@
-nombre_connexions=$(last | grep -c "lucas" | wc -l)
-date=$(date +'%d-%m-%Y-%H-%M')
+utilisateur="$lucas"
+nombre_connexions=$(last | grep "$utilisateur" | wc -l)
+
+date=$(date "+%d-%m-%Y-%H-%M")
 nom_fichier="number_connection-$date"
 
-echo "$nombre_connexions" > /home/lucas/shell-exe/job08/Backup/$nom_fichier
+echo "$nombre_connexions" > $nom_fichier
+
 tar -cvf /home/lucas/shell-exe/job08/Backup/$nom_fichier.tar $nom_fichier
+
+echo "L'utilisateur s'est connecté $nombre_connexions fois, le $date" 
+
+rm $nom_fichier
